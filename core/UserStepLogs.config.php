34daaef0dac73c1113b48d4e65caea3236fd02c7
emoretti85 <ettoremoretti27@gmail.com>
2015-04-30 06:48:17 -0700

    Create UserStepLogs.config.php

---
+++ core/UserStepLogs.config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Configurazione Database
+ * Your DB configuration
+ */
+define("DB_DSN", "mysql:host=localhost;dbname=UserStepLogs");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+
+define("USER_STEP_TABLE", "userstep_table");
